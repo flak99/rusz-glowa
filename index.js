fd8ca347500c7b1348b879c0e4293039ae2fa4b4
inż. Krystian Flak <flak99@o2.pl>
2024-12-15 19:44:01 +0100

    Pierwszy etap pisania logiki di gry w statki RG JS

---
+++ index.js
@@ -0,0 +1,26 @@
+let location1 = Math.floor(Math.random() * 5),
+  location2 = location1 + 1,
+  location3 = location2 + 1,
+  guess,
+  hits = 0,
+  guesses = 0,
+  isShunk = false;
+
+console.log(location1, location2, location3);
+while (!isShunk) {
+  guess = prompt("Podaj lokacje strzaly");
+
+  if (guess == location1 || guess == location2 || guess == location3) {
+    hits++;
+    alert("trafienie");
+  } else {
+    alert("pudło");
+  }
+  guesses++;
+  if (hits === 3) {
+    isShunk = true;
+    alert("statek zatopiopny");
+  }
+}
+
+console.log(` Liczba prób ${guesses}, na zatopienie statku`);
